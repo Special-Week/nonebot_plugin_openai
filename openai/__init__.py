e0c14eeac632e23c104321818be1daf01e532c64
Special-Week <2385612749@qq.com>
2022-12-08 22:59:23 +0800

    寄

--- openai/__init__.py
+++ openai/__init__.py
@@ -13,11 +13,13 @@ openai_text = on_command(
 async def _(msg: Message = CommandArg()):
     if api_key == "寄":
         await openai_text.finish("请先配置openai_api_key")
-    
-    await openai_text.send(MessageSegment.text("让本喵想想吧..."))
     prompt = msg.extract_plain_text()
     if prompt == "" or prompt == None or prompt.isspace():
         await openai_text.finish("需要提供文本prompt")
+    await openai_text.send(MessageSegment.text("让本喵想想吧..."))
     loop = asyncio.get_event_loop()
-    res = await loop.run_in_executor(None, get_openai_reply, prompt)
+    try:
+        res = await loop.run_in_executor(None, get_openai_reply, prompt)
+    except Exception as e:
+        await openai_text.finish(str(e))
     await openai_text.finish(MessageSegment.text(res))
